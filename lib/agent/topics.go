1f5c512a8c30ea967b7fb8385683836cad32c4da
Hannes Hansen <mail@hannesh.de>
2023-12-06 14:30:21 +0100

    add agent operator topic

--- lib/agent/topics.go
+++ lib/agent/topics.go
@@ -6,3 +6,11 @@ import (
 
 // Used by Agents to register
 const AgentsTopic = fog.FogAnalyticsTopicPrefix + "agents"
+
+func GetStartOperatorAgentTopic(agentID string) string {
+	return AgentsTopic + "/" + agentID + "control/start"
+}
+
+func GetStopOperatorAgentTopic(agentID string) string {
+	return AgentsTopic + "/" + agentID + "control/stop"
+}
