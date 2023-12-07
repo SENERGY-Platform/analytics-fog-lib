b9fcd16ec9e6743d4e4dc708cd3bcda16d987f56
Hannes Hansen <mail@hannesh.de>
2023-12-07 15:49:51 +0100

    updated downstream topics

--- lib/downstream/topic.go
+++ lib/downstream/topic.go
@@ -7,10 +7,18 @@ import (
 // Used by kafka2mqtt to send cloud operator results or device data downstream to fog environment  
 const DownstreamTopic = "/downstream"
 
-func GetDownstreamOperatorCloudTopic(id string) string {
+func GetDownstreamOperatorCloudSubTopic(id string) string {
 	return cloud.CloudAnalyticsMQTTTopicPrefix + id + DownstreamTopic + "/operator/#"
 }
 
-func GetDownstreamDeviceCloudTopic(id string) string {
+func GetDownstreamDeviceCloudSubTopic(id string) string {
 	return cloud.CloudAnalyticsMQTTTopicPrefix + id + DownstreamTopic + "/device/#"
+}
+
+func GetDownstreamOperatorCloudPubTopicPrefix(id string) string {
+	return cloud.CloudAnalyticsMQTTTopicPrefix + id + DownstreamTopic + "/operator/"
+}
+
+func GetDownstreamDeviceCloudPubTopicPrefix(id string) string {
+	return cloud.CloudAnalyticsMQTTTopicPrefix + id + DownstreamTopic + "/device/"
 }
