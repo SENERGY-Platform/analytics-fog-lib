08443ab10cda891b5e66fe1a2aa6df9f26b9bba5
Hannes Hansen <mail@hannesh.de>
2024-07-03 13:56:31 +0200

    fixed local device topic

--- lib/devices/topics.go
+++ lib/devices/topics.go
@@ -1,18 +1,6 @@
 package devices
 
-import (
-	"errors"
-
-	"github.com/SENERGY-Platform/analytics-fog-lib/lib/location"
-)
-
 const LOCAL_EVENT_TOPIC = "event/"
-func GetDeviceOutputTopic(deviceID, serviceID, deploymentLocation string) (string, error) {
-	if(deploymentLocation == location.Local) {
-		topic := LOCAL_EVENT_TOPIC + deviceID + "/" + serviceID
-		return topic, nil
-	} else if deploymentLocation == location.Cloud {
-		return serviceID, nil
-	}
-	return "", errors.New("Deployment Location not supported: " + deploymentLocation)
+func GetLocalDeviceOutputTopic(deviceLocalID, serviceName string) (string) {
+	return LOCAL_EVENT_TOPIC + deviceLocalID + "/" + serviceName
 }
