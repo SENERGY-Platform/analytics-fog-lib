5d43e7f70840515d4c623324eea2e055975a09e5
TZ <zsco@users.noreply.github.com>
2026-05-13 15:54:59 +0200

    fix broker config struct

--- lib/mqtt/config.go
+++ lib/mqtt/config.go
@@ -1,13 +1,17 @@
 package mqtt
 
 type BrokerConfig struct {
-	Host string
-	Port string
+	Host     string
+	Port     string
+	Username *string
+	Password *string
 }
 
 type FogBrokerConfig struct {
-	Host string `json:"broker_host" env_var:"FOG_BROKER_HOST"`
-	Port string `json:"broker_port" env_var:"FOG_BROKER_PORT"`
+	Host     string  `json:"broker_host" env_var:"FOG_BROKER_HOST"`
+	Port     string  `json:"broker_port" env_var:"FOG_BROKER_PORT"`
+	Username *string `json:"username" env_var:"FOG_BROKER_USERNAME"`
+	Password *string `json:"password" env_var:"FOG_BROKER_PASSWORD"`
 }
 
 type PlatformBrokerConfig struct {
